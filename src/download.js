3a22858a26b7462514f674d16d36bff6da9cb0af
umlx5h <umlx5h20@gmail.com>
2020-04-15 06:29:09 +0900

    add handshake handler

--- src/download.js
+++ src/download.js
@@ -1,9 +1,48 @@
 'use strict';
 
+const net = require('net');
+const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
+const message = require('./message');
 
 module.exports = torrent => {
   tracker.getPeersUdp(torrent, peers => {
     console.log("get peers success:", peers);
+    peers.forEach(peer => download(peer, torrent));
   });
 };
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on('data', recvBuf => {
+    // msgLen calculates the length of a whole message
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
+  });
+}
+
+function download(peer, torrent) {
+  const socket = net.Socket();
+  socket.on('error', console.log);
+  socket.connect(peer.port, peer.ip, () => {
+    socket.write(message.buildHandshake(torrent));
+  });
+  onWholeMsg(socket, msg => msgHandler(msg, socket));
+}
+
+function msgHandler(msg, socket) {
+  if (isHandshake(msg)) socket.write(message.buildInterested());
+}
+
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 &&
+         msg.toString('utf8', 1) === 'BitTorrent protocol';
+}
